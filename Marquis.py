2e2af874fbed688f607220bdb7d21d4c3f31b4e5
David <dmhacker@yahoo.com>
2014-05-02 21:08:53 -0700

    GTFO out of spawn nao!

--- Marquis.py
+++ Marquis.py
@@ -1,6 +1,12 @@
 import rg
 import random
 
+def sqdist(p1, p2):
+    return (p2[0] - p1[0]) ** 2 + (p2[1] - p1[1]) ** 2
+
+def deepCorner(loc):
+    return (abs(loc[0]-9), abs(loc[1]-9)) == (6, 6)
+
 '''
 Created April 30, 2014
 
@@ -8,7 +14,8 @@ They call me Monsieur the Marquis.
 
 Marquis is my first real attempt at good prediction. Its main strategy is to camp spawn locations.
 It also implements dynamic move and attack queues to tell friendlies and enemies to avoid certain areas
-Its best match against liquid 1.0 was: 36 - 42
+Its best match against liquid 1.0 was: 32 - 42
+Its best match against Plat10 was: 44 - 25
 
 I plan to adapt some of these methods to future bots while improving Marquis' attack + danger evaluation
 '''
@@ -19,14 +26,16 @@ class Robot:
     ally_locs = [] # Allied bots: [tuple]
     enemy_locs = [] # Enemy bots: [tuple]
     safe_spawnpoints = [] # List of spawnpoints to go to: [tuple]
-    marked_spawnpoints = []
     move_queue = [] # List of taken moves: [tuple]
     moving_allies = [] # List of allied locs that are moving: [tuple]
     attack_queue = [] # List of attacked locs: [tuple]
     bump_queue = [] # The stored queue of bumped bots (think history): [tuple]
+    urgent_queue = [] # Urgent locations that bots need to avoid
     bumped = [] # List of locs to be bumped: [tuple]
+    urgent = [] # List of locs to be bumped with a priority above regular: [tuple]
     tillspawn = 10 # Credit to liquid 1.0 for this
     board_locs = [] # Cached board locations
+    kill_me_now = []
     
     MIN_HEALTH = 15
     
@@ -44,42 +53,71 @@ class Robot:
         self.update(game)
         curr = self.location
         
+        if curr in self.kill_me_now:
+            self.kill_me_now.remove(curr)
+            return ['suicide']
+        
         if self.isTrapped(curr):
-            if self.tillspawn == 1 and self.isSpawn(curr):
+            print "URGENT:",curr,"is trapped!"
+            if self.tillspawn == 0 and self.isSpawn(curr):
+                print '- Nothing better to do'
                 return ['suicide']
             else:
-                bump_me = self.untrap(curr)
-                if bump_me not in self.moving_allies:
-                    if bump_me == None:
+                bump_me = self.unTrap(curr)
+                if bump_me == None or self.empty(bump_me) == False: # No allies to bump
+                    enemies = self.nearbyEnemies(curr)
+                    if len(enemies) == 0:
+                        print "- Wimping out and guarding"
                         return ['guard']
+                    else:
+                        # Don't add the attack location to the queue
+                        print "- I'm going down fighting!"
+                        try_kill = ()
+                        least_hp = 51
+                        for enemy in enemies:
+                            if self.botAt(enemy)['hp'] < least_hp:
+                                try_kill = enemy
+                        return ['attack', try_kill]
+                else: # YES, a bot is moving away!
+                    print "- Route opening up --> taking it:",bump_me
                     self.move_queue.append(bump_me)
                     self.moving_allies.append(curr)
                     return ['move',bump_me]
+                    
+        if curr in self.urgent_queue:
+            action = self.safeMove(curr)
+            if action[0] == 'guard':
+                self.urgent.append(curr)
+            return action
                 
-        if self.isSpawn(curr):
-            return self.flee(curr)
+        if self.isSpawn(curr) and self.tillspawn == 1:
+            "Performing stupid move out of spawn:",curr
+            bad_move = rg.toward(curr, rg.CENTER_POINT)
+            if self.isTrapped(bad_move, with_spawn=True) and len(self.nearbyEnemies(bad_move)) > 1:
+                "+ I see bots there to kill!"
+                self.kill_me_now.append(bad_move)
+            return ['move', bad_move]
         
-        # Enemies always take priority
         direct = self.nearbyEnemies(curr)
         amount_direct = len(direct)
         if amount_direct == 1:
             only_enemy = direct[0]
             if self.isSpawn(only_enemy) and (self.tillspawn == 0 or self.tillspawn == 9):
                 return ['attack', only_enemy]
-            if self.botAt(curr)['hp'] < self.MIN_HEALTH:
-                return self.flee(curr)
+            if self.botAt(curr)['hp'] < self.MIN_HEALTH and self.isSpawn(only_enemy) == False:
+                return self.safeMove(curr)
             helpers = self.nearbyAllies(only_enemy)
             helpers.remove(curr)
-            total_hp = self.nethp(helpers) + self.botAt(curr)['hp']
+            total_hp = self.netHP(helpers) + self.botAt(curr)['hp']
             if (self.botAt(only_enemy)['hp'] >= total_hp):
-                return self.flee(curr)
+                return self.safeMove(curr)
             else:
                 self.attack_queue.append(only_enemy)
                 return ['attack',only_enemy]
         elif amount_direct > 1:
             if amount_direct == 4:
                 return ['suicide']
-            return self.flee(curr)
+            return self.safeMove(curr)
         
         possible = self.nearbyEnemies(curr, radius=2, borderonly=True)
         amount_possible = len(possible)
@@ -89,17 +127,20 @@ class Robot:
                     guess = rg.toward(curr, possible[0])
                     self.attack_queue.append(guess)
                     return ['attack', guess]
-            return self.flee(curr)
+            return self.safeMove(curr)
         elif amount_possible > 1:
             if self.isSpawn(curr) and self.tillspawn <= 2:
-                return self.flee(curr)
+                return self.safeMove(curr)
             where_to_attack = self.bestAttack(curr)
             # one_enemy = possible[random.randint(0, amount_possible - 1)]
             self.attack_queue.append(where_to_attack)
             return ['attack', rg.toward(curr, where_to_attack)]
         
         if curr in self.bump_queue:
-            return self.flee(curr)
+            action = self.safeMove(curr)
+            if action[0] == 'guard':
+                self.bumped.append(curr)
+            return action
        
         if self.nearbyAllies(curr) == 4:
             bump_me = self.untrap(curr)
@@ -114,26 +155,28 @@ class Robot:
             self.attack_queue.append(attackloc)
             return ['attack',attackloc]
         
-        spawnMove = self.toward(curr, self.nearestCamp(curr))
-        if spawnMove is None:
-            return ['guard']
-        self.move_queue.append(spawnMove)
-        self.moving_allies.append(curr)
-        return ['move',spawnMove]
+        return self.safeMove(curr)
     
     def update(self, game):
         if game['turn'] is not self.turn:
             self.turn = game['turn']
             self.tillspawn = (10 - (self.turn % 10)) % 10
+            if self.turn > 90:
+                self.tillspawn = 10
             del self.move_queue[:]
             del self.attack_queue[:]
             
             del self.moving_allies[:]
+            
             del self.bump_queue[:]
             for bump_me in self.bumped:
                 self.bump_queue.append(bump_me)
             del self.bumped[:]
-            del self.marked_spawnpoints[:]
+            
+            del self.urgent_queue[:]
+            for gtfo in self.urgent:
+                self.urgent_queue.append(gtfo)
+            del self.urgent[:]
             
         del self.ally_locs[:]
         del self.enemy_locs[:]   
@@ -156,22 +199,21 @@ class Robot:
         danger_dict = {}
         for loc in locs:
             danger = 0
-            if self.isNearAnySpawnpoint(loc, 2):
-                turn_til_spawn = self.turn % 10
-                if turn_til_spawn == 9 or turn_til_spawn == 0:
-                    turn_danger = turn_til_spawn
-                    if turn_danger == 0:
-                        turn_danger = 10
-                    danger += 500 * (turn_danger + 1)
+            near_spawn = self.nearestSpawn(loc)
+            if near_spawn != None: #if self.isNearAnySpawnpoint(loc):
+                if self.tillspawn == 1:
+                    danger += 500
             if self.isSpawn(loc):
-                danger += 9000
+                danger += 499 * (10 - self.tillspawn)
             if loc in self.attack_queue:
-                danger += 5000
+                danger += 1000
+            if deepCorner(loc):
+                danger += 9000
             if len(self.nearbyEnemies(loc)) > 0:
                 danger += 500 * len(self.nearbyEnemies(loc))
             if len(self.nearbyEnemies(loc, 2, True)) > 0:
                 danger += 250 + (10 * len(self.nearbyEnemies(loc, 2, True)))
-            screw_me = len(self.nearbyAllies(loc, 1))
+            screw_me = len(self.nearbyAllies(loc))
             if screw_me > 0:
                 danger -= 25
             danger += screw_me * 25
@@ -182,15 +224,17 @@ class Robot:
                 danger += rg.wdist(loc, ally) * 5
             if self.empty(loc):
                 if self.isSpawn(loc):
-                    if self.tillspawn != 1:
+                    if self.tillspawn > 1:
                         danger_dict[loc] = danger
                 else:
+                    '''
                     count_me = True
-                    for potential in self.nearbySpawnpoints(loc, radius=1):
+                    for potential in self.nearbySpawnpoints(loc):
                         if potential in self.ally_locs:
                             count_me = False
                     if count_me:
-                        danger_dict[loc] = danger
+                    '''
+                    danger_dict[loc] = danger
         return danger_dict
     
     # TODO: Improve
@@ -203,7 +247,7 @@ class Robot:
             evaluation = 0
             # Net HP of nearby enemies?
             adjacent_enemies = self.nearbyEnemies(loc)
-            total_adj_hp = self.nethp(adjacent_enemies)
+            total_adj_hp = self.netHP(adjacent_enemies)
             evaluation += total_adj_hp
             if self.isSpawn(loc) and self.tillspawn <= 1:
                 evaluation += 500
@@ -214,7 +258,8 @@ class Robot:
             for adj in rg.locs_around(loc, filter_out=('invalid','obstacle')):
                 if self.isInEnemyCluster(adj, 3):
                     evaluation += 10
-            attack_dict[loc] = evaluation
+            if loc not in self.ally_locs:
+                attack_dict[loc] = evaluation
         return attack_dict
     
     def bestAttack(self, currLoc):
@@ -240,7 +285,7 @@ class Robot:
             return None
         return safe[0]
     
-    def flee(self, currLoc,appendme=True):
+    def safeMove(self, currLoc,appendme=True):
         safest = self.safe(currLoc)
         if safest == None:
             #print "\n Cannot find a safe location to flee to at:",currLoc
@@ -313,16 +358,6 @@ class Robot:
     def botAt(self, loc):
         return self.all_bots[loc]
     
-    def nearestCamp(self, location):
-        selected = rg.CENTER_POINT
-        least_dist = rg.settings.board_size ** 2
-        for loc in self.safe_spawnpoints:
-            dist = rg.wdist(location, loc)
-            if dist < least_dist: # and self.empty(location)
-                selected = loc
-                least_dist = dist
-        return selected
-    
     def nearestSpawn(self, location):
         aroundme = rg.locs_around(location, filter_out=('invalid', 'obstacle'))
         for a in aroundme:
@@ -345,7 +380,7 @@ class Robot:
                 initial = rg.dist(loc, s)
         return spawn
     
-    def nearbySpawnpoints(self, loc, radius=4):
+    def nearbySpawnpoints(self, loc, radius=1):
         spawn = []
         for s in self.safe_spawnpoints:
             if rg.wdist(loc, s) <= radius:
@@ -358,11 +393,11 @@ class Robot:
                 return True
         return False
     
-    def untrap(self, loc):
+    def unTrap(self, loc):
         nearby = rg.locs_around(loc, filter_out=('invalid','obstacle'))
         for near in nearby:
             if near in self.ally_locs:
-                self.bumped.append(near)
+                self.urgent.append(near)
                 return near
         return None
     
@@ -379,13 +414,23 @@ class Robot:
         return False
         
         
-    def isTrapped(self, loc):
+    def isTrapped(self, loc, with_spawn=False):
         trap = True
-        nearby = rg.locs_around(loc, filter_out=('invalid','obstacle'))
+        nearby = []
+        if with_spawn:
+            nearby = rg.locs_around(loc, filter_out=('invalid','obstacle','spawn'))
+        else:
+            nearby = rg.locs_around(loc, filter_out=('invalid','obstacle'))
         for near in nearby:
             if near not in self.all_bots:
                 trap = False
         return trap
+    
+    def nextToTrapped(self, loc):
+        for around in rg.locs_around(loc, filter_out=('invalid','obstacle')):
+            if self.isTrapped(around, with_spawn=True):
+                return True
+        return False
 
     # I take no credit for this code! Credit goes to: Rage MK1
     # I only optimized it by adding in danger evaluation
@@ -428,7 +473,7 @@ class Robot:
             return safest
         return None 
     
-    def nethp(self, locs):
+    def netHP(self, locs):
         net = 0
         for loc in locs:
             bot = self.all_bots[loc]
